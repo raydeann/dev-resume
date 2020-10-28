38b39c6c37c6b9750bc4a3875ec4283e452268e3
maddygupt <73608900+maddygupt@users.noreply.github.com>
2020-10-29 01:21:45 +0530

    Update

--- public/script.js
+++ public/script.js
@@ -1,15 +1,20 @@
 const filterInput = document.querySelector("input");
 
 const filterFunc = () => {
+  
   const container = document.querySelectorAll(".container");
+  
   for (let i = 0; i < container.length; i++) {
     let p = container[i].querySelector(".name");
     if (
       p.innerHTML.toUpperCase().indexOf(filterInput.value.toUpperCase()) > -1
+      
     ) {
       container[i].style.display = "";
-    } else {
+    } 
+    else {
       container[i].style.display = "none";
+      
     }
   }
 };
